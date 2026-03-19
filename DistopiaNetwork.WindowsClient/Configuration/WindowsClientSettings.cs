bce863c3469be7a243d35fd0c93f484e360b6740
Sergio Villarreal <sergiovillarreal@libero.it>
2026-03-19 16:11:27 +0100

    pdpdl#

--- DistopiaNetwork.WindowsClient/Configuration/WindowsClientSettings.cs
+++ DistopiaNetwork.WindowsClient/Configuration/WindowsClientSettings.cs
@@ -4,7 +4,7 @@ public class WindowsClientSettings
 {
     public const string Section = "WindowsClientSettings";
 
-    public string ServerUrl { get; set; } = "http://localhost:5000";
+    public string ServerUrl { get; set; } = "http://localhost:55689";
     public string ServerId { get; set; } = "server-a";
     public string PrivateKeyPath { get; set; } = "publisher_private.key";
     public string PublicKeyPath { get; set; } = "publisher_public.key";
